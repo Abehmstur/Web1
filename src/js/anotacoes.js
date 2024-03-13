075b1aff99ceedc4d766cb688d5ce46d936e43e6
Abehmstur <matheus.medeiros970@gmail.com>
2024-03-13 20:12:04 -0300

    Aula DIVS TABELAS 13-03-2024

---
+++ src/js/anotacoes.js
@@ -0,0 +1,7 @@
+//css
+/*
+dado que você tem uma divida limitada em width e height
+e o texto incluso nela ultrapassar o tamanho.
+Você pode adicionar a tag OVERFLOW: scroll 
+para que exista uma barra de rolagem nessa div e o texto fique contido dentor dela apenas.
+*/
